b8be02103d95125bc18f9b71eb9e9ce9d8b52f21
PaRi29 <87933597+PaRi29@users.noreply.github.com>
2024-11-12 23:53:41 +0100

    sleep after the order delete

--- alpaca_dividend_strategy/main_dividend.py
+++ alpaca_dividend_strategy/main_dividend.py
@@ -200,6 +200,7 @@ class DividendTradingSimulator:
                     shares_bought = self.budget // (self.open_price)
                     limit_price= self.open_price*0.98
                     rounded_limit_price = round(limit_price, 2)
+
                     self.is_position_closed = self.close_buy_position_pre_hours(self.stock_to_buy, shares_bought, rounded_limit_price)
                     time.sleep(10)
                     if self.is_position_closed:
@@ -250,13 +251,14 @@ class DividendTradingSimulator:
                     logging.info(wait_time)
                     time.sleep(wait_time)
                     time.sleep(1)
+                    self.ALPACA_API.cancel_all_orders()
+                    time.sleep(5)
 
                     self.open_price = float(self.get_stock_price_intraday(self.stock_to_buy))
                     shares_bought = self.budget // (self.open_price)
                     limit_price= self.open_price*0.98
                     rounded_limit_price = round(limit_price, 2)
 
-                    self.ALPACA_API.cancel_all_orders()
                     self.is_position_closed = self.close_buy_position_pre_hours(self.stock_to_buy, shares_bought, rounded_limit_price)
                     time.sleep(10)
                     self.is_short_open = self.short_sell_pre_hours(self.stock_to_buy, shares_bought, rounded_limit_price)
@@ -269,13 +271,14 @@ class DividendTradingSimulator:
                     logging.info(wait_time)
                     time.sleep(wait_time)
                     time.sleep(1)
+                    self.ALPACA_API.cancel_all_orders()
+                    time.sleep(5)
 
                     self.open_price = float(self.get_stock_price_intraday(self.stock_to_buy))
                     shares_bought = self.budget // (self.open_price)
                     limit_price= self.open_price*0.98
                     rounded_limit_price = round(limit_price, 2)
                     
-                    self.ALPACA_API.cancel_all_orders()
                     self.is_short_open = self.short_sell_pre_hours(self.stock_to_buy, shares_bought, rounded_limit_price)
 
             sell_time = self.get_next_time(hour=15, minute=32)
