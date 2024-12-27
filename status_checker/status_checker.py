6289de66dee091c1be99cde7fb729ad8924dde7a
PaRi29 <87933597+PaRi29@users.noreply.github.com>
2024-12-27 13:34:16 +0100

    Update status_checker.py

--- status_checker/status_checker.py
+++ status_checker/status_checker.py
@@ -33,9 +33,8 @@ class ProcessMonitor:
             )
             return process_name in result.stdout
         except Exception as e:
-            logging.info(f"Error checking process: {e}")
+            print(f"Error checking process: {e}")
             return False
-
     def monitor_processes(self, processes_to_monitor):
         """
         Continuously monitor the specified processes.
@@ -46,15 +45,15 @@ class ProcessMonitor:
                 if not self.is_process_running(process_name):
                     all_running = False
                     alert_message = f"Alert: Process {process_name} is not running!"
-                    logging.info(f"{time.ctime()}: {alert_message}")
+                    print(f"{time.ctime()}: {alert_message}")
                     self.telegram_bot_sendtext(alert_message)
                     break  # Exit the loop to avoid multiple alerts in the same hour
             
             if not all_running:
-                logging.info(f"{time.ctime()}: Sleeping for 1 hour before re-checking.")
+                print(f"{time.ctime()}: Sleeping for 1 hour before re-checking.")
                 time.sleep(3600)  # Sleep for 1 hour
             else:
-                logging.info(f"{time.ctime()}: All processes are running.")
+                print(f"{time.ctime()}: All processes are running.")
                 time.sleep(60)  # Sleep for 1 minute if all processes are fine
 
 
@@ -64,7 +63,10 @@ if __name__ == "__main__":
 
     TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
     TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
-    PROCESSES_TO_MONITOR = ["main_dividend.py", "main_short.py"]
+    PROCESSES_TO_MONITOR = [
+        "../alpaca_dividend_strategy/main_dividend.py",
+        "../alpaca_option_strategy/option_research.py"
+    ]
 
     logging.basicConfig(filename='status_checker.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
     monitor = ProcessMonitor(TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID)
